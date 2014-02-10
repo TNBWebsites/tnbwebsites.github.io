e0531874fc0a0cf32cbe94815be8736b837e1051
Kevin Lee <kevin.c.lee.sf@gmail.com>
2014-02-10 12:47:49 -0800

    Fixed navigation + JS comments

--- js/behaviour.js
+++ js/behaviour.js
@@ -1,16 +1,20 @@
+/* Offsetting the scroll sensors (because of the fixed nav) */
 var scrollOffset = 60;
 
+/* Easier pushState */
 var maybePushState = function(url){
 	if (history.pushState) history.pushState(null, null, url);
 }
 
 $(function(){
+	/* Easier preventDefault */
 	if($(".js-preventOnClick").length > 0) {
 		$(".js-preventOnClick").on("click", function(e) {
 			e.preventDefault();
 		});
 	}
 
+	/* Popover on the contact menu entry */
 	$('#contact').popover({
 		placement: "bottom",
 		title: "To get in touch with us:",
@@ -19,30 +23,40 @@ $(function(){
 		container: 'body'
 	});
 
-	if($(".js-scrollTo").length > 0) {
-		$(".js-scrollTo").on("click", function(e) {
-			var targetStr = $(this).attr("href");
-			$("html, body").animate({ scrollTop: $(targetStr).offset().top - scrollOffset });
-			maybePushState(targetStr);
-			$("header li.active").removeClass("active");
-			$(this).parent().addClass("active");
-			e.preventDefault();
-		});
-	}
-
-	if($("body#home .navbar-brand").length > 0) {
-		$("body#home .navbar-brand").on("click", function(e) {
-			$("html, body").animate({ scrollTop: 0 });
-			$("header li.active").removeClass("active");
-			maybePushState("index.html");
-			e.preventDefault();
-		});
-	}
-
+	/** INTERNAL NAVIGATION ON HOMEPAGE */
 	if($("body#home").length > 0) {
-		$('body').scrollspy({ target: '#js-scrollspy', offset: scrollOffset+50 })
-		$('#js-scrollspy').on('activate.bs.scrollspy', function(){
+
+		/* Handling the click on the nav */
+		if($(".js-scrollTo").length > 0) {
+			$(".js-scrollTo").on("click", function(e) {
+				var targetStr = $(this).attr("href");
+				$("html, body").animate({ scrollTop: $(targetStr).offset().top - scrollOffset });
+				maybePushState(targetStr);
+				$("header li.active").removeClass("active");
+				$(this).parent().addClass("active");
+				e.preventDefault();
+			});
+		}
+
+		/* Handling the click on the logo */
+		if($(".navbar-brand").length > 0) {
+			$(".navbar-brand").on("click", function(e) {
+				$("html, body").animate({ scrollTop: 0 });
+				$("header li.active").removeClass("active");
+				maybePushState("index.html");
+				e.preventDefault();
+			});
+		}
+
+		/* Handling the scrollspy (changing state and nav when user scrolls) */
+		$('body').scrollspy({ target: '#js-scrollspy', offset: scrollOffset+50 }) // changing nav
+		$('#js-scrollspy').on('activate.bs.scrollspy', function(){ // changing state
 			maybePushState($("header li.active a").attr("href"));
 		});
+
+		/* Handling the user that arrives with an anchored URL */
+		if (window.location.hash.length>1) {
+			setTimeout(function(){$("html, body").scrollTop($(window.location.hash).offset().top - scrollOffset)}, 500);
+		}
 	}
 })
